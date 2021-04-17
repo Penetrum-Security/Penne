2b6c5bcb0d82115f1dd0af0f345fa8e8c0fa0169
oldkingcone <11233163+oldkingcone@users.noreply.github.com>
2021-04-17 12:26:50 -0400

    small updates
    small updates

--- penne/quarantine/db_create.py
+++ penne/quarantine/db_create.py
@@ -4,8 +4,9 @@ import traceback
 from termcolor import cprint
 
 
-con = sqlite3.connect("penne/data/strainer.db")
+con = sqlite3.connect("data/strainer.sqlite")
 cursed = con.cursor()
+
 
 def first_run():
     try:
@@ -19,9 +20,12 @@ def first_run():
         con.execute('''
         CREATE TABLE IF NOT EXISTS penne_pasta(
                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                       date DATETIME default CURRENT_TIMESTAMP, 
+                       date DATETIME default CURRENT_TIMESTAMP,
+                       original_name TEXT NOT NULL DEFAULT '-',
+                       sample_name TEXT NOT NULL DEFAULT '-',
                        sample_origin TEXT NOT NULL DEFAULT '-', 
-                       sample_blob BLOB NOT NULL
+                       sample_blob BLOB NOT NULL,
+                       encrypted TEXT NOT NULL DEFAULT 'true'
         )
                        ''')
         con.execute('''
@@ -49,6 +53,28 @@ def first_run():
         }
 
 
-def check_updates(date_today):
-    if date_today is not None or isinstance(date_today, str):
-        cprint("[ !! ] CHECKING FOR UPDATES [ !! ]", "blue", "on_white", attrs=['dark', 'bold'])
+def check_updates(updated_url, pull_from_git):
+    if updated_url is not None or isinstance(updated_url, str) and isinstance(pull_from_git, bool):
+        cprint("[ !! ] CHECKING FOR UPDATES [ !! ]", "white", "on_blue", attrs=['dark', 'bold'])
+        try:
+            cursed.execute('''INSERT INTO penne_pulls(last_pull_url, updated_from_github) VALUES ('%s', '%s')''' % (updated_url, pull_from_git))
+            con.commit()
+        except sqlite3.OperationalError as e:
+            cprint("[ !! ] THERE WAS AN ERROR CONENCTING TO THE DATABASE, BUILDING AND/OR REBUILDING. [ !! ]",
+                   "red", "on_white", attrs=['dark', 'bold'])
+            first_run()
+            cursed.execute('''INSERT INTO penne_pulls(last_pull_url, updated_from_github) VALUES ('%s', '%s')''' % (updated_url, pull_from_git))
+            con.commit()
+    else:
+        cprint("[ !! ] COULD NOT CHECK FOR UPDATES [ !! ]", "red", "on_white", attrs=['dark', 'bold'])
+
+
+def insert_blob(blob_data, blob_name, where_found, original_name, encrypted, need_to_upload):
+    if isinstance(blob_data, str) and isinstance(blob_name, str) and isinstance(where_found, str) and isinstance(original_name, str) and isinstance(encrypted, bool):
+        cursed.execute('''INSERT INTO penne_pasta(original_name, sample_name, sample_origin, sample_blob, encrypted) VALUES ('%s', '%s', '%s', '%s', '%s')''' % (original_name, blob_name, where_found, blob_data, encrypted))
+    elif isinstance(need_to_upload, bool) and need_to_upload is True:
+        cprint("[ !! ] UNKNOWN SAMPLE IS BEING UPLOADED, PLEASE WAIT. [ !! ]", "red", "on_white", attrs=['dark', 'bold'])
+        return {
+            'Upload': True,
+            'UploadDest': ''
+        }
--- penne/quarantine/noodler.py
+++ penne/quarantine/noodler.py
@@ -2,20 +2,26 @@ import base64
 import pathlib
 from Crypto.Cipher import AES
 import secrets
+import datetime as dt
+from penne.quarantine.db_create import check_updates
 
+check_updates('https://github.com/Penetrum-Security/Penne', 5)
 
 def spicy_file(path, filename, detection_type, arch):
     print()
 
+
 def cold_file(sqlitedb, uploaded, encrypted):
     if isinstance(encrypted, bool) and isinstance(uploaded, bool):
-        if encrypted and not uploaded:
+        if encrypted and not uploaded and sqlitedb:
             print()
-        elif not encrypted and uploaded:
+        elif not encrypted and uploaded and not sqlitedb:
             print()
         else:
             print()
 
+
 def check_prem():
     print()
+
 
