b6d4d70961e9782aeb5ca1d6bca242763a87f193
oldkingcone <11233163+oldkingcone@users.noreply.github.com>
2021-04-20 17:11:35 -0400

    added sig routine

--- penne/quarantine/db_create.py
+++ penne/quarantine/db_create.py
@@ -7,7 +7,6 @@ from termcolor import cprint
 
 from penne.lib.settings import HOME
 
-
 penne_json = load(open("{}/penne.json".format(HOME), "r"))
 penne_db = "{}/{}".format(penne_json['config']['penne_folders']['database_folder'].format(HOME), "strainer.sqlite")
 con = sqlite3.connect(penne_db)
@@ -78,22 +77,25 @@ def check_updates(updated_url, pull_from_git, is_premium):
         if is_premium is not None:
             cprint("[ !! ] CHECKING FOR UPDATES [ !! ]", "red", attrs=['bold'])
             try:
-                cursed.execute('''INSERT INTO penne_pulls(last_pull_url, updated_from_github, premium) VALUES (?, ?, ?)''',
-                               (updated_url, pull_from_git, is_premium,))
+                cursed.execute(
+                    '''INSERT INTO penne_pulls(last_pull_url, updated_from_github, premium) VALUES (?, ?, ?)''',
+                    (updated_url, pull_from_git, is_premium,))
                 con.commit()
             except sqlite3.OperationalError as e:
                 cprint("[ !! ] THERE WAS AN ERROR CONENCTING TO THE DATABASE, BUILDING AND/OR REBUILDING. [ !! ]",
                        "red", attrs=['dark'])
                 first_run()
-                cursed.execute('''INSERT INTO penne_pulls(last_pull_url, updated_from_github, premium) VALUES (?, ?, ?)''',
-                               (updated_url, pull_from_git, is_premium,))
+                cursed.execute(
+                    '''INSERT INTO penne_pulls(last_pull_url, updated_from_github, premium) VALUES (?, ?, ?)''',
+                    (updated_url, pull_from_git, is_premium,))
                 con.commit()
     else:
         cprint("[ !! ] COULD NOT CHECK FOR UPDATES [ !! ]", "red", attrs=['dark', 'bold'])
 
 
 def insert_blob(blob_data, blob_name, where_found, original_name, encrypted, need_to_upload, nonce, key, detected_as):
-    if isinstance(blob_data, str) and isinstance(blob_name, str) and isinstance(where_found, str) and isinstance(original_name, str) and isinstance(encrypted, bool) and isinstance(detected_as, str):
+    if isinstance(blob_data, str) and isinstance(blob_name, str) and isinstance(where_found, str) and isinstance(
+            original_name, str) and isinstance(encrypted, bool) and isinstance(detected_as, str):
         if key is None and nonce is None:
             return "Key and Nonce cannot be null"
         else:
@@ -101,29 +103,35 @@ def insert_blob(blob_data, blob_name, where_found, original_name, encrypted, nee
                 '''INSERT INTO penne_pasta(detected_as, original_name, sample_name, sample_origin, sample_blob, encrypted, stored_key, stored_nonce) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
                 (detected_as, original_name, blob_name, where_found, blob_data, encrypted, key, nonce,))
     elif isinstance(need_to_upload, bool) and need_to_upload is True:
-        cprint("[ !! ] UNKNOWN SAMPLE IS BEING UPLOADED, PLEASE WAIT. [ !! ]", "red", "on_white", attrs=['dark', 'bold'])
+        cprint("[ !! ] UNKNOWN SAMPLE IS BEING UPLOADED, PLEASE WAIT. [ !! ]", "red", "on_white",
+               attrs=['dark', 'bold'])
         return {
             'Upload': True,
             'UploadDest': ''
         }
 
 
-
 def create_sig_table():
     for files in os.listdir(penne_json['config']['penne_folders']['unzipped_sigs'].format(HOME)):
-        full_path = "{0}/{1}".format(penne_json['config']['penne_folders']['unzipped_sigs'].format(HOME), files)
-        with open(full_path) as in_sig:
-            for lines in in_sig.readlines():
-                split_sig = lines.split(':')
-                try:
-                    cursed.execute('''INSERT INTO penne_sigs(os, bytes_read, warning_type, sig, sha_hash) VALUES(?, ?, ?, ?, ?)''',
-                                   (
-                                       split_sig[1],
-                                       split_sig[2],
-                                       split_sig[3],
-                                       split_sig[4],
-                                       split_sig[5],
-                                   ))
-                    con.commit()
-                except sqlite3.IntegrityError as e:
-                    cprint("{}\nOffending Hash ->{}\n".format(e, split_sig[5]), "red", attrs=["dark"])
+        if files.endswith('pasta'):
+            full_path = "{0}/{1}".format(penne_json['config']['penne_folders']['unzipped_sigs'].format(HOME), files)
+            with open(full_path) as in_sig:
+                for lines in in_sig.readlines():
+                    split_sig = lines.split(':')
+                    try:
+                        cursed.execute(
+                            '''INSERT INTO penne_sigs(os, bytes_read, warning_type, sig, sha_hash) VALUES(?, ?, ?, ?, ?)''',
+                            (
+                                split_sig[1],
+                                split_sig[2],
+                                split_sig[3],
+                                split_sig[4],
+                                split_sig[5],
+                            ))
+                        con.commit()
+                    except sqlite3.IntegrityError as e:
+                        cprint("{}\nOffending Hash ->{}\n".format(e, split_sig[5]), "red", attrs=["dark"])
+        else:
+            cprint(
+                "[ ++ ] Appears as though a zip file or directory made its way into here... losin my noodle... [ ++ ]",
+                "red", attrs=['dark'])
