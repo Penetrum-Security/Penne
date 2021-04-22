1ea70adcb56ee47e7e140294d99c382582e07960
Ekultek <ekultek@noreply.github.com>
2021-04-22 16:44:54 -0500

    ???

--- penne/__main__.py
+++ penne/__main__.py
@@ -17,44 +17,45 @@ from penne.lib.settings import (
 
 
 def main():
-    try:
-        opts = Parser().optparse()
-        verify_args(opts)
+    opts = Parser().optparse()
+    verify_args(opts)
 
-        if opts.initialize:
-            if os.path.exists(HOME):
-                log.error("already initialized")
-                sys.exit(1)
-            log.info("initializing the database")
-            init()
-            log.info("done, rerun Penne")
+    if opts.initialize:
+        if os.path.exists(HOME):
+            log.error("already initialized")
             sys.exit(1)
-        if opts.scanner:
-            log.info("starting scan on directory: {}".format(
-                opts.startDir if opts.startDir != "." else "current directory"
-            ))
-            scan(
-                opts.startDir,
-                display_only_infected=opts.displayOnlyInfected,
-                threads=opts.threadNum,
-                move_detected=opts.moveFiles
-            )
-        if opts.sigtool:
-            if os.path.isdir(opts.filename):
-                files = ["{}/{}".format(opts.filename, f) for f in os.listdir(opts.filename)]
-                log.info("generating a total of {} signature(s) for files in {}".format(len(files), opts.filename))
-            else:
-                log.info("generating a signature for passed file: {}".format(opts.filename))
-                files = [opts.filename]
-            for f in files:
-                if not os.path.isdir(f):
-                    make_signature(
-                        f, byte_size=opts.byteSize, os_filler=opts.osFiller, no_save_sig=opts.noSaveSig,
-                        warn_type=opts.warnType
-                    )
-    except Exception:
+        log.info("initializing the database")
         init()
+        log.info("done, rerun Penne")
+        sys.exit(1)
+    if opts.scanner:
+        log.info("starting scan on directory: {}".format(
+            opts.startDir if opts.startDir != "." else "current directory"
+        ))
+        scan(
+            opts.startDir,
+            display_only_infected=opts.displayOnlyInfected,
+            threads=opts.threadNum,
+            move_detected=opts.moveFiles
+        )
+    if opts.sigtool:
+        if os.path.isdir(opts.filename):
+            files = ["{}/{}".format(opts.filename, f) for f in os.listdir(opts.filename)]
+            log.info("generating a total of {} signature(s) for files in {}".format(len(files), opts.filename))
+        else:
+            log.info("generating a signature for passed file: {}".format(opts.filename))
+            files = [opts.filename]
+        for f in files:
+            if not os.path.isdir(f):
+                make_signature(
+                    f, byte_size=opts.byteSize, os_filler=opts.osFiller, no_save_sig=opts.noSaveSig,
+                    warn_type=opts.warnType
+                )
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        log.warning("program is not intialized, starting initialization")
+        init()
