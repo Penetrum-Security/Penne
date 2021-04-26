814094567ae428f3b7d95802e64b87483d7689cb
oldkingcone <11233163+oldkingcone@users.noreply.github.com>
2021-04-25 22:10:08 -0400

    Update noodler.py

--- penne/quarantine/noodler.py
+++ penne/quarantine/noodler.py
@@ -8,13 +8,10 @@ import os.path
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import ChaCha20_Poly1305
 from termcolor import cprint
-from penne.quarantine.db_create import check_updates
 from penne.lib.settings import (
     log,
     HOME
 )
-
-# check_updates('https://github.com/Penetrum-Security/Penne', True, False)
 
 
 def spicy_file(path, filename, detection_type, arch, detected_as):
